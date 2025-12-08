49976bdd03e4db4de520d0a06acbea81aed65157
Sanket Sathe <sanketsth95@gmail.com>
2025-12-09 00:09:50 +0530

    Logout redirection ishhue

--- backend/logout.php
+++ backend/logout.php
@@ -17,6 +17,6 @@ if (ini_get("session.use_cookies")) {
 session_destroy();
 
 // Redirect to login page with logout message
-header('Location: ../login.php?logout=1');
+header('Location: ../Login.php?logout=1');
 exit();
 ?>
