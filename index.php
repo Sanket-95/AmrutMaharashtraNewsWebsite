275097b06eaad4f0dac960e66c19711bd0f10bac
Sanket Sathe <sanketsth95@gmail.com>
2025-12-15 01:15:09 +0530

    Error ra

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-// include 'components/header.php';
-// include 'components/navbar.php';
-// include 'components/db_config.php';
-include __DIR__ . '/components/header.php';
-include __DIR__ . '/components/navbar.php';
-include __DIR__ . '/components/db_config.php';
+include 'components/header.php';
+include 'components/navbar.php';
+include 'components/db_config.php';
+// include __DIR__ . '/components/header.php';
+// include __DIR__ . '/components/navbar.php';
+// include __DIR__ . '/components/db_config.php';
 
 
 // Fetch top news for carousel
