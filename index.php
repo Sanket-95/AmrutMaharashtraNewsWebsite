08bab72cecd2526bb9567de96329501257e3d533
Sanket Sathe <sanketsth95@gmail.com>
2025-12-15 01:00:06 +0530

    Error rack

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // include 'components/header.php';
 // include 'components/navbar.php';
 // include 'components/db_config.php';
