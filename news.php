707fd3e5cf9dd9b1da7cdf0ea127dff1c8671d24
Sanket Sathe <sanketsth95@gmail.com>
2026-02-10 23:42:58 +0530

    bug

--- news.php
+++ news.php
@@ -28,7 +28,7 @@ $query = "SELECT
     published_date,
     view
 FROM news_articles 
-WHERE news_id = ? and is_approved = 1 DATE(published_date) <= CURDATE()";
+WHERE news_id = ? and is_approved = 1";
 
 $stmt = $conn->prepare($query);
 $stmt->bind_param("i", $news_id);
