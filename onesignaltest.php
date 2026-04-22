684169c7403ec4226142b4d8c0792f99cd9d5df5
Sanket Sathe <sanketsth95@gmail.com>
2026-04-22 10:17:47 +0530

    s

--- onesignaltest.php
+++ onesignaltest.php
@@ -1,33 +1,24 @@
 <?php
 
 $app_id = "210aaacd-2369-4cb7-99c8-8b777f3d0f23";
-$rest_api_key = "wgtf5klwbubi5kchw7nngcyef";
-
-$url = "https://onesignal.com/api/v1/notifications";
-
-$data = [
-    "app_id" => $app_id,
-    "included_segments" => ["All"],
-    "contents" => ["en" => "Final FIXED notification"]
-];
+$rest_api_key = "bvpstnckseoufjqtmy6qifha2";
 
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 
-// ✅ CORRECT AUTH FORMAT
-$auth = base64_encode($rest_api_key . ":");
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
+    "app_id" => $app_id,
+    "included_segments" => ["All"],
+    "contents" => ["en" => "Final test after key reset"]
+]));
 
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    'Content-Type: application/json; charset=utf-8',
-    'Authorization: Basic ' . $auth
+    "Content-Type: application/json; charset=utf-8",
+    "Authorization: Basic " . $rest_api_key
 ]);
-
-curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
 $response = curl_exec($ch);
 
